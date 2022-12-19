5a08ede2dd4ffc878bc2177f778012f314d6321b
pablojjuarez567 <pablojjuarez567@gmail.com>
2022-12-19 22:40:07 +0100

    entregar examen

--- src/main/java/com/example/examenfranciscointerfaces/Ejercicio1.java
+++ src/main/java/com/example/examenfranciscointerfaces/Ejercicio1.java
@@ -1,5 +1,6 @@
 package com.example.examenfranciscointerfaces;
 
+import javafx.animation.Animation;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -7,10 +8,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class Ejercicio1 implements Initializable {
@@ -80,68 +83,15 @@ public class Ejercicio1 implements Initializable {
                         String sexo = sexoField.getValue();
                         String actividad = actividadField.getValue();
 
-                        int ger = 0;
-                        int get = 0;
+                        Persona persona = new Persona(nombre, edad, sexo, peso, altura, actividad);
 
-                        if(sexo.equals("Hombre")) {
-                                ger = (int) (66.4730 + (13.7516 * peso) + (5.0033 * altura) - (6.755 * edad));
-
-                                switch (actividad){
-                                        case "Muy ligera":
-                                                get = (int) (ger * 1.3);
-                                                break;
-                                        case "Ligera":
-                                                get = (int) (ger * 1.6);
-                                                break;
-                                        case "Moderada":
-                                                get = (int) (ger * 1.7);
-                                                break;
-                                        case "Intensa":
-                                                get = (int) (ger * 2.1);
-                                                break;
-                                }
-                        } else if(sexo.equals("Mujer")) {
-                                ger = (int) (655.0955 + (9.5634 * peso) + (1.8496 * altura) - (4.6756 * edad));
-
-                                switch (actividad){
-                                        case "Muy ligera":
-                                                get = (int) (ger * 1.3);
-                                                break;
-                                        case "Ligera":
-                                                get = (int) (ger * 1.5);
-                                                break;
-                                        case "Moderada":
-                                                get = (int) (ger * 1.6);
-                                                break;
-                                        case "Intensa":
-                                                get = (int) (ger * 1.9);
-                                                break;
-                                }
-                        }
-
-
-                        Persona persona = new Persona(nombre, edad, sexo, peso, altura, actividad, ger, get);
-
-                        ObservableList<Persona> personas = FXCollections.observableArrayList();
-                        tabla.setItems(data);
-                        tabla.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-                        tabla.getColumns().addAll(fileNameCol, pathCol, sizeCol, dateCol);
-
-                        tabla.setEditable(true);
                         tabla.getItems().add(persona);
-
-
-
-
-
-
+                        textoEditable.setText("Persona añadida");
 
                         nombreField.clear();
                         sexoField.setValue(null);
                         actividadField.setValue(null);
-
                 }
-
         }
 
         @Override
@@ -152,11 +102,25 @@ public class Ejercicio1 implements Initializable {
                 pesoField.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(0, 200, 0));
                 alturaField.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 250, 0));
 
-                TableView<Persona> table = new TableView<Persona>();
-                final ObservableList<Persona> data = FXCollections.observableArrayList(
-                        new Persona("Pablo", 5, "Hombre", 5, 5, "Muy ligera", 5, 5)
-                );
 
+                ArrayList<Persona> personas = new ArrayList<>();
+                personas.add(new Persona("Francisco", 39, "Hombre", 70, 180, "Moderada"));
+                personas.add(new Persona("Paca", 25, "Mujer", 50, 150, "Moderada"));
+                personas.add(new Persona("Pepe", 20, "Hombre", 100, 180, "Ligera"));
+                personas.add(new Persona("Juan", 30, "Hombre", 90, 200, "Muy ligera"));
+
+                ObservableList<Persona> listaPersonas = FXCollections.observableArrayList(personas);
+
+                columnaNombre.setCellValueFactory(new PropertyValueFactory("nombre"));
+                columnaEdad.setCellValueFactory(new PropertyValueFactory("edad"));
+                columnaSexo.setCellValueFactory(new PropertyValueFactory("sexo"));
+                columnaPeso.setCellValueFactory(new PropertyValueFactory("peso"));
+                columnaAltura.setCellValueFactory(new PropertyValueFactory("altura"));
+                columnaActividad.setCellValueFactory(new PropertyValueFactory("actividad"));
+                columnaGER.setCellValueFactory(new PropertyValueFactory("ger"));
+                columnaGET.setCellValueFactory(new PropertyValueFactory("get"));
+
+                tabla.setItems(listaPersonas);
         }
 
 
--- src/main/java/com/example/examenfranciscointerfaces/Persona.java
+++ src/main/java/com/example/examenfranciscointerfaces/Persona.java
@@ -9,16 +9,112 @@ public class Persona {
     private String actividad;
     private int ger;
     private int get;
-    public Persona(String nombre, int edad, String sexo, double peso, Integer altura, String actividad, int ger, int get) {
+    public Persona(String nombre, int edad, String sexo, double peso, Integer altura, String actividad) {
         this.nombre = nombre;
         this.edad = edad;
         this.sexo = sexo;
         this.peso = peso;
         this.altura = altura;
         this.actividad = actividad;
-        this.ger = ger;
-        this.get = get;
+
+        if(sexo.equals("Hombre")) {
+            this.ger = (int) (66.4730 + (13.7516 * peso) + (5.0033 * altura) - (6.755 * edad));
+
+            switch (actividad){
+                case "Muy ligera":
+                    this.get = (int) (ger * 1.3);
+                    break;
+                case "Ligera":
+                    this.get = (int) (ger * 1.6);
+                    break;
+                case "Moderada":
+                    this.get = (int) (ger * 1.7);
+                    break;
+                case "Intensa":
+                    this.get = (int) (ger * 2.1);
+                    break;
+            }
+        } else if(sexo.equals("Mujer")) {
+            this.ger = (int) (655.0955 + (9.5634 * peso) + (1.8496 * altura) - (4.6756 * edad));
+
+            switch (actividad){
+                case "Muy ligera":
+                    this.get = (int) (ger * 1.3);
+                    break;
+                case "Ligera":
+                    this.get = (int) (ger * 1.5);
+                    break;
+                case "Moderada":
+                    this.get = (int) (ger * 1.6);
+                    break;
+                case "Intensa":
+                    this.get = (int) (ger * 1.9);
+                    break;
+            }
+        }
     }
 
+    public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public Integer getAltura() {
+        return altura;
+    }
+
+    public void setAltura(Integer altura) {
+        this.altura = altura;
+    }
+
+    public String getActividad() {
+        return actividad;
+    }
+
+    public void setActividad(String actividad) {
+        this.actividad = actividad;
+    }
+
+    public int getGer() {
+        return ger;
+    }
+
+    public void setGer(int ger) {
+        this.ger = ger;
+    }
+
+    public int getGet() {
+        return get;
+    }
+
+    public void setGet(int get) {
+        this.get = get;
+    }
 }
